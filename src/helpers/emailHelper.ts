204abab45d3caea346fc35978d199bc4bec971e0
Muhammad Ranju <mdranju23@gmail.com>
2025-11-12 05:15:30 +0600

    feat: added email server

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -6,6 +6,7 @@ import { ISendEmail } from '../types/email';
 const transporter = nodemailer.createTransport({
   host: config.email.host,
   port: Number(config.email.port),
+  service: 'gmail',
   secure: false,
   auth: {
     user: config.email.user,
